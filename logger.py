717068384a7a84f0b907f9551cbc9611fbaac4a7
Elena <kor-09-06-2008@yandex.ru>
2023-06-21 23:16:40 +0300

    Добавлен код

--- logger.py
+++ logger.py
@@ -17,7 +17,7 @@ def input_data():
                     f"Выберите номер варианта: "))
 
     while var != 1 and var != 2:
-        print('Ты дурак?! Даю тебе последний шанс')
+        print("Ты дурак?! Даю тебе последний шанс")
         var = int(input("Введите номер варианта: "))
 
     if var == 1:
@@ -29,7 +29,7 @@ def input_data():
 
 
 def print_data():
-    print('Вывожу данные для Вас данные из 1-ого файла\n')
+    print("Вывожу данные для Вас данные из 1-ого файла\n")
     with open('data_first_variant.csv', 'r', encoding='utf-8') as file:
         data_first = file.readlines()
         data_first_version_second = []
@@ -48,64 +48,6 @@ def print_data():
     return data_first, data_second
 
 
-def change_line(dataFile, numberRow, numberFile):
-    answer = input(f"Изменить данную запись\n{dataFile[numberRow]}?\nВведите ответ: ")
-    while answer != 'да':
-        numberRow = int(input('Введите номер записи: ')) - 1
-
-    print(f"Меняем данную запись\n{dataFile[numberRow]}\n")
-    if numberFile == 1:
-        name = dataFile[numberRow].split('\n')[0]
-        surname = dataFile[numberRow].split('\n')[1]
-        phone = dataFile[numberRow].split('\n')[2]
-        address = dataFile[numberRow].split('\n')[3]
-    if numberFile == 2:
-        name = dataFile[numberRow].split(';')[0]
-        surname = dataFile[numberRow].split(';')[1]
-        phone = dataFile[numberRow].split(';')[2]
-        address = dataFile[numberRow].split(';')[3]
-
-    answer = int(input(f"Какие данные Вы хотите поменять?\n"
-                       f"1. Имя\n"
-                       f"2. Фамилия\n"
-                       f"3. Номер телефона\n"
-                       f"4. Адрес\n"
-                       f"Введите ответ: "))
-    while answer < 1 or answer > 4:
-        print("Вы ошиблись!\nВведите корректный номер от 1 до 4")
-        answer = int(input(f"Какие данные Вы хотите поменять?\n"
-                           f"1. Имя\n"
-                           f"2. Фамилия\n"
-                           f"3. Номер телефона\n"
-                           f"4. Адрес\n"
-                           f"Введите ответ: "))
-    if answer == 1:
-        name = name_data()
-    elif answer == 2:
-        surname = surname_data()
-    elif answer == 3:
-        phone = phone_data()
-    elif answer == 4:
-        address = address_data()
-
-    if numberFile == 1:
-        data_first = dataFile[:numberRow] + [f'{name}\n{surname}\n{phone}\n{address}'] + \
-                     dataFile[numberRow + 1:]
-        if numberRow + 1 == len(dataFile):
-            data_first = dataFile[:numberRow] + [f'{name}\n{surname}\n{phone}\n{address}\n']
-        with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
-            file.write(''.join(data_first))
-        print('Изменения успешно сохранены!')
-    else:
-        data_second = dataFile[:numberRow] + [f'{name};{surname};{phone};{address}'] + \
-                      dataFile[numberRow + 1:]
-        if numberRow + 1 == len(dataFile):
-            data_second = dataFile[:numberRow] + [f'{name};{surname};{phone};{address}\n']
-        with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
-            file.write(''.join(data_second))
-        print('Изменения успешно сохранены!')
-
-
 def put_data():
     print('Из какого файла Вы хотите изменить данные?')
     data_first, data_second = print_data()
@@ -119,13 +61,63 @@ def put_data():
         print("Какую именно запись по счету Вы хотите изменить?")
         number_journal = int(input('Введите номер записи: '))
         number_journal -= 1
-        change_line(data_first, number_journal, 1)
+        print(f'Изменить данную запись\n{data_first[number_journal]}')
+    
+        data_temp=data_first[number_journal].split()
+        num_element = int(input("Что Вы хотите изменить: 1) Имя 2) Фамилию 3) Номер телефона 4) Город 5) Всю запись"))
+
+        if num_element == 1:
+            data_first = data_first[:number_journal] + [f'{name_data()}\n{data_temp[1]}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                data_first[number_journal + 1:]
+        
+        elif num_element == 2:
+            data_first = data_first[:number_journal] + [f'{data_temp[0]}\n{surname_data()}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                data_first[number_journal + 1:]
+        
+        elif num_element == 3:
+            data_first = data_first[:number_journal] + [f'{data_temp[0]}\n{data_temp[1]}\n{phone_data()}\n{data_temp[3]}\n'] + \
+                data_first[number_journal + 1:]
+        
+        elif num_element == 4:
+            data_first = data_first[:number_journal] + [f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{address_data()}\n'] + \
+                data_first[number_journal + 1:]
+        
+        elif num_element == 5:
+            data_first = data_first[:number_journal] + [f'{name_data()}\n{surname_data()}\n{phone_data()}\n{address_data()}\n'] + \
+                         data_first[number_journal + 1:]
+
+
+        with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
+            file.write(''.join(data_first))
+        print('Изменения успешно сохранены!')
+    
     else:
         print("Какую именно запись по счету Вы хотите изменить?")
         number_journal = int(input('Введите номер записи: '))
         number_journal -= 1
-        change_line(data_second, number_journal, 2)
+        print(f'Изменить данную запись\n{data_second[number_journal]}')
+        data_temp=data_second[number_journal].split(';')
+        num_element = int(input("Что Вы хотите изменить: 1) Имя 2) Фамилию 3) Номер телефона 4) Город 5) Всю запись "))
 
+        if  num_element == 1:
+            data_second = data_second[:number_journal] + [f'{name_data()};{data_temp[1]};{data_temp[2]};{data_temp[3]}\n'] + \
+                          data_second[number_journal + 1:]
+        elif num_element == 2:
+            data_second = data_second[:number_journal] + [f'{data_temp[0]};{surname_data()};{data_temp[2]};{data_temp[3]}\n'] + \
+                          data_second[number_journal + 1:]
+        elif num_element == 3:
+            data_second = data_second[:number_journal] + [f'{data_temp[0]};{data_temp[1]};{phone_data()};{data_temp[3]}\n'] + \
+                          data_second[number_journal + 1:]
+        elif num_element == 4:
+            data_second = data_second[:number_journal] + [f'{data_temp[0]};{data_temp[1]};{data_temp[2]};{address_data()}\n'] + \
+                      data_second[number_journal + 1:]
+        elif num_element  == 5:
+            data_second = data_second[:number_journal] + [f'{name_data()};{surname_data()};{phone_data()};{address_data()}\n'] + \
+                      data_second[number_journal + 1:]
+
+        with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
+            file.write(''.join(data_second))
+        print('Изменения успешно сохранены!')  # Можно вывести конечные данные
 
 def delete_data():
     print('Из какого файла Вы хотите удалить данные?')
@@ -141,8 +133,25 @@ def delete_data():
         number_journal = int(input('Введите номер записи: '))
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Удалить данную запись\n{data_first[number_journal - 1]}')
-        # print(data_first[:number_journal - 1], data_first[number_journal + 1:]) error
-        data_first = data_first[:number_journal - 1] + data_first[number_journal + 1:]
+        data_temp = data_first[number_journal].split()
+        num_element = int(input("Что Вы хотите удалить: 1) Имя 2) Фамилию 3) Номер телефона 4)Город 5) Всю запись"))
+
+        if num_element == 1:
+            data_first = data_first[:number_journal] + [f'{""}\n{data_temp[1]}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif num_element == 2:
+            data_first = data_first[:number_journal] + [f'{data_temp[0]}\n{""}\n{data_temp[2]}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif num_element == 3:
+            data_first = data_first[:number_journal] + [f'{data_temp[0]}\n{data_temp[1]}\n{""}\n{data_temp[3]}\n'] + \
+                         data_first[number_journal + 1:]
+        elif num_element == 4:
+            data_first = data_first[:number_journal] + [f'{data_temp[0]}\n{data_temp[1]}\n{data_temp[2]}\n{""}\n'] + \
+                         data_first[number_journal + 1:]
+        elif num_element == 5:
+            data_first = data_first[:number_journal] + data_first[number_journal + 1:]
+
+
         with open('data_first_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_first))
         print('Изменения успешно сохранены!')
@@ -151,7 +160,27 @@ def delete_data():
         number_journal = int(input('Введите номер записи: '))
         # Можно добавить проверку, чтобы человек не выходил за пределы записи
         print(f'Удалить данную запись\n{data_second[number_journal - 1]}')
-        data_second = data_second[:number_journal] + data_second[number_journal + 1:]
+        print(data_second)
+        data_temp = data_second[number_journal-1].split(';')
+        num_element = int(input("Что Вы хотите удалить: 1) Имя 2) Фамилию 3) Номер телефона 4) Город 5) Всю запись"))
+
+        if num_element == 1:
+            data_second = data_second[:number_journal-1] + [f'{""};{data_temp[1]};{data_temp[2]};{data_temp[3]}'] + \
+                          data_second[number_journal:]
+        elif num_element == 2:
+            data_second = data_second[:number_journal-1] + [f'{data_temp[0]};{""};{data_temp[2]};{data_temp[3]}\n'] + \
+                          data_second[number_journal:]
+        elif num_element == 3:
+            data_second = data_second[:number_journal-1] + [f'{data_temp[0]};{data_temp[1]};{""};{data_temp[3]}\n'] + \
+                          data_second[number_journal:]
+        elif num_element == 4:
+            data_second = data_second[:number_journal-1] + [f'{data_temp[0]};{data_temp[1]};{data_temp[2]};{""}\n'] + \
+                          data_second[number_journal:]
+        elif num_element == 5:
+            data_second = data_second[:number_journal] + data_second[number_journal + 1:]
+
+        init =input('pause')
         with open('data_second_variant.csv', 'w', encoding='utf-8') as file:
             file.write(''.join(data_second))
-        print('Изменения успешно сохранены!')  # Можно вывести конечные данные
+        print("Изменения успешно сохранены!")  # Можно вывести конечные данные
+
